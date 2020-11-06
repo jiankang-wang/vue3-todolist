2be1c7ba80dae04b3ee694a058a485e6508de989
wangjiankang <1159085983@qq.com>
2020-11-06 14:42:48 +0800

    style: edit & delete

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -23,13 +23,44 @@
       <small class="form-text text-muted">回车即可加入</small>
     </div>
     <div class="list-group">
-      <li class="list-group-item" v-for="(item, index) in todos" :key="index">{{ item }}</li>
+      <li
+        @click.stop="check(item)"
+        class="list-group-item d-flex align-item-center justify-content-between"
+        v-for="(item, index) in todos"
+        :key="index"
+      >
+        <div class="form-check">
+          <input
+            type="checkbox"
+            :id="item.id"
+            class="formcheckinput"
+            :checked="item.state ===TodoItemState.DONE"
+            :disabled="item.state === TodoItemState.DELETE"
+          />
+          <label
+            @click.stop.prevent="check(item)"
+            :for="item.id"
+            :class="{ 'text-black-50 line-through': item.state === TodoItemState.DONE }"
+          >
+            {{ item }}
+          </label>
+        </div>
+        <div
+          class="float-right ctrls"
+          :class="{'d-none': item.state !== TodoItemState.OPEN}"
+        >
+          <button type="button" class="btn btn-warning btn-sm mr-2 text-light">编辑</button>
+          <button type="button" class="btn btn-danger btn-sm">删除</button>
+        </div>
+      </li>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import store from '@/store'
+import { TodoItemState } from '@/common/const'
+import { TodoItem } from '@/common/interfacce'
 import { defineComponent, ref, reactive, computed } from 'vue'
 
 export default defineComponent({
@@ -39,15 +70,33 @@ export default defineComponent({
       store.commit('add', inputValue)
       inputValue.value = ''
     }
+    const check = (item: TodoItem) => {
+      store.commit('check', item.id)
+    }
     return reactive({
       inputValue,
       add,
-      todos: computed(() => store.state.todos)
+      todos: computed(() => store.state.todos),
+      TodoItemState,
+      check
     })
   }
 })
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+  .line-through {
+    text-decoration: line-through
+  }
+  .list-group-item {
+    user-select: none;  // 禁止选中文本
+    .ctrls {
+      display: none
+    }
+    &:hover {
+      .ctrls {
+        display: block
+      }
+    }
+  }
 </style>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,9 @@
-import { createStore } from 'vuex'
+import { createStore, createLogger } from 'vuex'
 import { TodoItem } from '@/common/interfacce'
 import { TodoItemState } from '@/common/const'
 import { v4 as uuidv4 } from 'uuid'
+
+const debug = process.env.NODE_ENV !== 'production'
 
 export default createStore({
   state: {
@@ -14,10 +16,15 @@ export default createStore({
         text: value,
         state: TodoItemState.OPEN
       })
+    },
+    check (state, id) {
+      const index = state.todos.findIndex((item) => item.id === id)
+      state.todos[index].state = state.todos[index].state === TodoItemState.DONE ? TodoItemState.OPEN : TodoItemState.DONE
     }
   },
   actions: {
   },
   modules: {
-  }
+  },
+  plugins: debug ? [createLogger()] : []
 })
